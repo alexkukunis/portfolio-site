d1c04e81942e0c1dd4e895762048adc2b20b1116
Alex Kukunis <alexkukunis@gmail.com>
2026-04-20 16:08:32 -0400

    Prisma v7 migration: move URL to config and clean up client instantiation

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,7 +14,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-// @ts-ignore
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
